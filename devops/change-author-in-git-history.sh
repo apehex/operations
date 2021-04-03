f5863bb9891963f68103a7fc3c2ccd8e5cd9f570
apehex <apehex@protonmail.com>
2021-04-03 16:59:31 +0000

    Fix typos

--- devops/change-author-in-git-history.sh
+++ devops/change-author-in-git-history.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
-
-OLD_EMAIL="your-old-email@example.com"
-CORRECT_NAME="Your Correct Name"
-CORRECT_EMAIL="your-correct-email@example.com"
-
+git filter-branch -f --env-filter '
+OLD_EMAIL="old@mail.com"
+CORRECT_NAME="apehex"
+CORRECT_EMAIL="apehex@protonmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
