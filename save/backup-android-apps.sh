1cd0c046c02bd5c3b3f46e784f1298b7498bf19c
apehex <apehex@protonmail.com>
2021-04-20 20:03:26 +0200

    Backup all the android apk, split config included

---
+++ save/backup-android-apps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# output directory
+OUTPUT=${1:-$(dirname "$0")}
+
+# operate in the tmp dir
+CACHE=$(mktemp -d)
+
+# check whether there's a connected device
+
+# list the package names only
+adb shell pm list packages -f -3 | awk -F'=' -ne '{print $NF}' > "$CACHE/packages.lst"
+
+# loop on the packages
+while IFS= read -r package; do
+  # show progress
+  echo "======================================== $package";
+  # tidy: group the sources & config by package
+  mkdir "$CACHE/$package" && cd "$CACHE/$package";
+  # import the modules
+  adb </dev/null shell pm path "$package" | awk -F':' -ne '{print $NF}' > "$CACHE/$package/modules.lst";
+  # import each module apk
+  while read module; do
+    adb pull "$module";
+  done < "$CACHE/$package/modules.lst"
+  # extract the sources
+  #apktool d -o sources base.apk
+done < "$CACHE/packages.lst"
+
+# sync with the output directory
+rsync -avh --update --progress "$CACHE" "$OUTPUT"
+
+# clean tmp
+rm -rfv "$CACHE"
