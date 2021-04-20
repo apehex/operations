de46a5f8cc95a5b632dc2a113f743e018c5cc922
apehex <17317183+apehex@users.noreply.github.com>
2021-04-20 20:25:26 +0200

    Reverse the apk files, fix typos

--- save/backup-android-apps.sh
+++ save/backup-android-apps.sh
@@ -9,7 +9,7 @@ CACHE=$(mktemp -d)
 # check whether there's a connected device
 
 # list the package names only
-adb shell pm list packages -f -3 | awk -F'=' -ne '{print $NF}' > "$CACHE/packages.lst"
+adb shell pm list packages -f -3 | awk -F'=' -e '{print $NF}' > "$CACHE/packages.lst"
 
 # loop on the packages
 while IFS= read -r package; do
@@ -18,17 +18,19 @@ while IFS= read -r package; do
   # tidy: group the sources & config by package
   mkdir "$CACHE/$package" && cd "$CACHE/$package";
   # import the modules
-  adb </dev/null shell pm path "$package" | awk -F':' -ne '{print $NF}' > "$CACHE/$package/modules.lst";
+  adb </dev/null shell pm path "$package" | awk -F':' -e '{print $NF}' > "$CACHE/$package/modules.lst";
   # import each module apk
   while read module; do
+    # pull the apk
     adb pull "$module";
+    # reverse to sources
+    filename=$(echo "$module" | awk -F'/' -e '{print $NF}');
+    apktool d -o "$CACHE/$package/sources/$filename" "$CACHE/$package/$filename";
   done < "$CACHE/$package/modules.lst"
-  # extract the sources
-  #apktool d -o sources base.apk
 done < "$CACHE/packages.lst"
 
 # sync with the output directory
 rsync -avh --update --progress "$CACHE" "$OUTPUT"
 
 # clean tmp
-rm -rfv "$CACHE"
+rm -rf "$CACHE"
