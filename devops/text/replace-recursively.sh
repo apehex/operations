8ed74f317da65f9800e2c959974910d37adc1b51
apehex <apehex@protonmail.com>
2022-01-14 20:20:21 +0100

    Add another search & replace with Perl

--- devops/text/replace-recursively.sh
+++ devops/text/replace-recursively.sh
@@ -1,5 +1,10 @@
 # ignore the .git directory entirely
 find ./ \
-\( -type d -name .git -prune \) -o \
--type f -print0 |
-xargs -0 sed -i 's/before/after/g'
+    \( -type d -name .git -prune \) -o \
+    -type f -print0 |
+    xargs -0 sed -i 's/before/after/g'
+
+# using Perl
+perl -p -i -e 's/oldstring/newstring/g' `grep -ril oldstring *`
+
+perl -p -i -e 's/industrious.optimize/engineering.optimize/g' `grep -ril 'industrious.optimize' industrious`
