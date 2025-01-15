6873b9184f6b576910d6e632191ab64dea6051b7
apehex <apehex@protonmail.com>
2025-01-15 21:33:52 +0100

    Fix typo

--- devops/git/substitude-text-in-history.sh
+++ devops/git/substitude-text-in-history.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # the substitions are listed line by line, with the format:
-# regex:old===>new
+# regex:old==>new
 # regex:password=\w+==>password=3x4mpl3!
 
 # rewrite the whole git history
