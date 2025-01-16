a1ad3730e188e32837392056a2ddb9b1bcfc28f1
apehex <17317183+apehex@users.noreply.github.com>
2025-01-16 15:27:23 +0100

    Add alternative methods to remove unwanted data from the git history

--- devops/git/substitude-text-in-history.sh
+++ devops/git/substitude-text-in-history.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # the substitions are listed line by line, with the format:
+# specificstring==>specificsubstitution
 # regex:old==>new
 # regex:password=\w+==>password=3x4mpl3!
 
+# BFG reverts its changes in the HEAD commit so that the current files are unchanged
+touch dummy & git add dummy
+git commit -m 'Empty commit'
+
 # rewrite the whole git history
-bfg --replace-text substitions.txt .
+java -jar bfg.jar --replace-text ../substitutions.txt .
+
+# ignore the revert commit, which does not contain any meaningful changes
+git reset HEAD^
+git restore .
