6b20bced6bff0a4290e6920a17b3a17f2651b548
apehex <17317183+apehex@users.noreply.github.com>
2026-02-17 14:10:31 +0100

    Clean the cache of "git-filter-repo" too.

--- clean/remove-secrets-from-git-history.sh
+++ clean/remove-secrets-from-git-history.sh
@@ -10,10 +10,10 @@ jq -r '.[].Secret' < /tmp/report.json |
     perl -pe 's/$/==>**REDACTED**/' > /tmp/textmap.txt # concatenate into a regex
 
 # remove the API keys from the file contents
-git filter-repo --replace-text /tmp/textmap.txt .
+git filter-repo --replace-text /tmp/textmap.txt
 
 # cleanup artifacts that may still hold the secrets
-rm -rf .git/refs/original .git/logs .git/filter-branch # remove backups and logs
+rm -rf .git/refs/original .git/logs .git/filter-branch .git/filter-repo # remove backups and logs
 git for-each-ref --format='%(refname)' 'refs/original' | xargs -r -n1 git update-ref -d # remove backup refs
 git reflog expire --expire=now --all # expire any remaining reflogs
 git gc --prune=now --aggressive # prune unreachable objects
